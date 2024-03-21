7767bda1305e2ccb26cee5b219e5cefc1aab062f
muhammadsukran <muhammadsukran@student.uir.ac.id>
2024-03-21 15:04:44 +0700

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+   <h1> Hello World</h1>
+    <Buttonc />
+    <Buttonc />
+
+  </div>
+  </template>
+
+
+  <style scoped>
+  .btn {
+    background-color: red;
+    color: white;
+    font-size: 28px;
+  }
+</style>
+
+<!-- vue 3 -->
+<script setup>
+  import Buttonc from './components/Buttonc.vue'
+</script>
---
+++ vue-project/src/components/icons/TextHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Text Heder
+    </h1>
+</template>
